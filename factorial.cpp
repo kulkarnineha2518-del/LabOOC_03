fa8d12338983dd48dceb9198f4e4ec4dbf0e7664
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-18 11:24:19 +0000

    factorial using while loop

--- factorial.cpp
+++ factorial.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int factorial = 1;
+
+    cout << "Enter a positive number: ";
+    cin >> number;
+
+    while (number > 1) {
+        factorial = factorial * number;
+        number = number - 1;
+    }
+
+    cout << "Factorial is: " << factorial << endl;
+
+    return 0;
+}
 
