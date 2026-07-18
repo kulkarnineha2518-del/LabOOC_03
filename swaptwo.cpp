fc9063d41b1d64b6a017bced5ac5fc81f8e7b39f
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-18 09:16:19 +0000

    swap with out thie

--- swaptwo.cpp
+++ swaptwo.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter first positive integer (a): ";
+    cin >> a;
+
+    cout << "Enter second positive integer (b): ";
+    cin >> b;
+
+    a = a + b;   
+    b = a - b;   
+    a = a - b;   
+
+    cout << "After swapping:" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
 
