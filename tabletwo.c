0ec7749e0d3949054fe602ede05096a4e680bd32
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-23 16:28:41 +0000

    two table

--- tabletwo.c
+++ tabletwo.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n = 2; // Change this number to see a different table
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}
 
