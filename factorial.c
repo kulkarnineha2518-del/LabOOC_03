ef690ab56c09396d9f093e3ac7a91fbee687e2f4
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-23 16:20:20 +0000

    force commit

--- factorial.c
+++ factorial.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Change this to find other factorials
+    int fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d is %d", n, fact);
+    return 0;
+}
 
