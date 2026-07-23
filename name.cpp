79c85a853836c243f1a56701dbce20fe75d2f3f5
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-23 14:16:45 +0000

    display name class division

--- name.cpp
+++ name.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
+using namespace std;
+int main() 
+{
+    printf("Name: Neha Kulkarni\n");
+    printf("Roll No: 03\n");
+    printf("Class: B.Tech Computer Science And Engineering\n");
+    printf("Division: C\n");
+
+    return 0;
+}
 
