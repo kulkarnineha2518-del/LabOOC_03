06cf29f792f8466604abb84be130664aff0ead39
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-24 07:13:33 +0000

    muldiv

---
+++ muldiv.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+void mult()
+ {
+    nt num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "The product is: " << (num1 * num2) << "\n\n";
+}
+
+
+void division()
+ {
+    double num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "The result is: " << (num1 / num2) << "\n\n";
+    }
