fa81831226d25c811558e13bc0506790c06e6a04
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-24 07:06:20 +0000

    sum and sub

---
+++ sumsub.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+void add() 
+{
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "The sum is: " << (num1 + num2) << "\n\n";
+}
+
+ 
+void sub() 
+{
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "The subtraction is: " << (num1 - num2) << "\n\n";
+}
+
+
