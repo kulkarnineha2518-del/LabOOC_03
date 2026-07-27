7ade728c2ea432f7c79249b99a2872998e2d2cbd
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-27 15:26:37 +0000

    reverse a no

--- reverseno.cpp
+++ reverseno.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, r;
+    int rev = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n > 0) {
+        r = n % 10;       // Get the last digit
+        rev = rev * 10 + r; // Add it to the reversed number
+        n = n / 10;       // Remove the last digit
+    }
+
+    cout << "Reversed: " << rev;
+
+    return 0;
+}
 
