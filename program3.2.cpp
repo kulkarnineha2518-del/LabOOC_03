d50050890b536fcec0e59c3934f2a7ebb4df6d70
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-07-31 06:41:25 +0000

    use of new and delete operators

--- program3.2.cpp
+++ program3.2.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *arr;
+    int size;
+    cout<<"Enter the size of integer array:";
+    cin>>size;
+    cout<<"Creating an array of size "<<size<<"..";
+    arr = new int[size];
+    cout<<"\nDynamic allocation of memory for array arr is successful.";
+     int sum = 0;
+    cout << "Enter " << size << " numbers:\n";
+    for(int i = 0; i < size; i++) 
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
 
+    cout << "The sum of the entered numbers is: " << sum << "\n";
+
+
+    delete arr;
+    return 0;
+}
