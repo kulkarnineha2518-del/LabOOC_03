2c185b56a954432e7b5b4d5230d24a58dc5b36a5
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-08-07 07:26:17 +0000

    defining the employee details using class and obje ct

--- employee.cpp
+++ employee.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Employee {
+    public:
+    int employeeId;
+    string branch;
+    string designation;
+    float basicSalary;
+    float grossSalary;
+
+    void inputData() {
+        cout << "Enter Employee ID: ";
+        cin >> employeeId;
+        
+        cout << "Enter Branch Name: ";
+        getline(cin >> ws, branch);
+        
+        cout << "Enter Designation: ";
+        getline(cin, designation);
+        
+        cout << "Enter Basic Salary: ";
+        cin >> basicSalary;
+        
+        grossSalary = basicSalary + (0.20f * basicSalary) + (0.10f * basicSalary);
+    }
+
+    void displayData() const {
+        cout << "\n       Employee Details       \n";
+        cout << "==============================\n";
+        cout << "Employee ID  : " << employeeId << endl;
+        cout << "Branch       : " << branch << endl;
+        cout << "Designation  : " << designation << endl;
+        cout << "Basic Salary : $" << basicSalary << endl;
+        cout << "Gross Salary : $" << grossSalary << endl;
+    }
+};
+
+int main() {
+    Employee emp;
+    emp.inputData();
+    emp.displayData();
+    return 0;
+}
