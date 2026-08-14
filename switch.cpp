95abf991467370f85de2bad689a01f79ff3149b3
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-08-14 05:16:01 +0000

    switch case program

--- switch.cpp
+++ switch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+// Function declarations
 void add(int num1, int num2);
 void sub(int num1, int num2);
 void mult(int num1, int num2);
@@ -11,6 +11,7 @@ int main() {
     int num1, num2;
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
+    
     for (int i = 1; i <= 4; i++) 
     {
         switch (i) 
@@ -19,6 +20,7 @@ int main() {
                 mult(num1, num2);
                 break;
             case 2:
+                // Implicit conversion from int to double happens here automatically
                 division(num1, num2);
                 break;
             case 3:
@@ -30,4 +32,25 @@ int main() {
         }
     }
     return 0;
+}
+
+// Function definitions (Missing in original code)
+void add(int num1, int num2) {
+    cout << "Addition: " << num1 + num2 << endl;
+}
+
+void sub(int num1, int num2) {
+    cout << "Subtraction: " << num1 - num2 << endl;
+}
+
+void mult(int num1, int num2) {
+    cout << "Multiplication: " << num1 * num2 << endl;
+}
+
+void division(double num1, double num2) {
+    if (num2 == 0) {
+        cout << "Division: Error! Division by zero." << endl;
+    } else {
+        cout << "Division: " << num1 / num2 << endl;
+    }
 }
