2d1c85978e4e59435035df1f3bcfaed4e7c9dd25
kulkarnineha2518-del <kulkarnineha2518@gmail.com>
2026-08-21 04:39:22 +0000

    calculates time in hr and min

--- timecalculator.cpp
+++ timecalculator.cpp
@@ -1,71 +1,61 @@
-#include <iostream> 
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-class Time { 
-    private: 
+class Time {
+    public:
     int hours;
-    int minutes; 
+    int minutes;
     
-    public: 
     // Simple constructor
     Time(int h = 0, int m = 0) {
         hours = h;
         minutes = m;
-        
-        // If minutes are 60 or more, convert them into hours
-        if (minutes >= 60) {
-            hours = hours + 1;
-            minutes = minutes - 60;
-        }
-    } 
+    }
     
     // Simple add function
-    Time add(const Time &t) { 
-        int total_hours = hours + t.hours;
-        int total_minutes = minutes + t.minutes;
+    Time add(Time t) { 
+        int total_h = hours + t.hours;
+        int total_m = minutes + t.minutes;
         
-        return Time(total_hours, total_minutes); 
-    } 
-    
-    // Simple subtract function
-    Time subtract(const Time &t) { 
-        int total_minutes1 = (hours * 60) + minutes;
-        int total_minutes2 = (t.hours * 60) + t.minutes;
+        // If minutes cross 60, change them to 1 hour
+        if (total_m >= 60) {
+            total_h = total_h + 1;
+            total_m = total_m - 60;
+        }
         
-        int difference_minutes = total_minutes1 - total_minutes2;
-        
-        // Convert total remaining minutes back into hours and minutes
-        int final_hours = difference_minutes / 60;
-        int final_minutes = difference_minutes % 60;
-        
-        return Time(final_hours, final_minutes); 
-    } 
+        return Time(total_h, total_m); 
+    }
     
     // Simple display function
-    void display() const { 
+    void display() { 
         cout << hours << " Hours and " << minutes << " Minutes" << endl; 
-    } 
-}; 
+    }
+};
 
-int main() { 
-    // Using simple values where t1 is smaller and t2 is larger
-    Time t1(4, 20); 
-    Time t2(7, 10); 
+int main() {
+    int h1, m1, h2, m2;
     
-    Time sum = t1.add(t2); 
-    Time diff = t2.subtract(t1); 
+    // Taking input for Time 1
+    cout << "Enter Time 1 Hours: ";
+    cin >> h1;
+    cout << "Enter Time 1 Minutes: ";
+    cin >> m1;
     
-    cout << "First Time: "; 
-    t1.display(); 
+    // Taking input for Time 2
+    cout << "Enter Time 2 Hours: ";
+    cin >> h2;
+    cout << "Enter Time 2 Minutes: ";
+    cin >> m2;
     
-    cout << "Second Time: "; 
-    t2.display(); 
+    // Creating the Time objects with user inputs
+    Time t1(h1, m1);
+    Time t2(h2, m2);
     
-    cout << "Addition: "; 
-    sum.display(); 
+    // Adding and showing the result
+    Time result = t1.add(t2);
     
-    cout << "Subtraction: "; 
-    diff.display(); 
+    cout << "\nTotal Combined Time: ";
+    result.display();
     
-    return 0; 
+    return 0;
 }
